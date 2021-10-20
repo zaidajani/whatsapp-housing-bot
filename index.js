5c3a55153cbb5785913245ef2f4800e42287f32e
zaidajani <zaidajani2007@gmail.com>
2021-10-20 16:51:23 +0530

    some functionalities added

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ wa.create().then(client => start(client));
 function start(client) {
   axios.get(url).then((res) => {
     const summary = res.data;
-    const data = `${summary.house}, ${summary.state}, ${summary.city}, ${summary.address}, ${summary.zip}, ${summary.owner}, ${summary.phone}`
     client.onMessage(async message => {
       if (message.body === 'Hi') {
-        await client.sendText(message.from, `Hi \nAvailable homes: \n${JSON.stringify(data)}`);
-        // await client.sendText(message.from, "hi");
+        await client.sendText(message.from, "Hi, check out our available homes near mumbai!")
+        let i;
+        for (i = 0; i < summary.length; i++) {
+          const data = `${summary[i].house}, ${summary[i].state}, ${summary[i].city}, ${summary[i].address}, ${summary[i].zip}, ${summary[i].owner}, ${summary[i].phone}`
+          await client.sendText(message.from, `Hi \nHome ${i+1}: \n${data}`);
+        }
       }
     });
   });
