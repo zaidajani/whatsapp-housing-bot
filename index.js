286f0ad0125fce123622c15cad2980c3c512dbfd
zaidajani <zaidajani2007@gmail.com>
2021-10-24 21:17:09 +0530

    made the API more interactive

--- index.js
+++ index.js
@@ -7,14 +7,25 @@ wa.create().then(client => start(client));
 function start(client) {
   axios.get(url).then((res) => {
     const summary = res.data;
+    client.onMessage(async msg => {
+      await client.sendText(message.from, 'Hi, welcome to whatsapp housing.\nTo view available homes in mumbai, type 1\nTo talk to an expert, type 2\nTo sell your house, type 3.');
+    });
     client.onMessage(async message => {
-      if (message.body === 'Hi') {
-        await client.sendText(message.from, "Hi, check out our available homes near mumbai!")
+      if (message.body === '1') {
+        await client.sendText(message.from, "Check out homes available near mumbai.")
         let i;
         for (i = 0; i < summary.length; i++) {
           const data = `${summary[i].house}, ${summary[i].state}, ${summary[i].city}, ${summary[i].address}, ${summary[i].zip}, ${summary[i].owner}, ${summary[i].phone}`
-          await client.sendText(message.from, `Hi \nHome ${i+1}: \n${data}`);
+          await client.sendText(message.from, `Home ${i+1}: \n${data}`);
         }
+        await client.sendText(message.from, "Thanks");
+        return;
+      }
+      else if (message.body === '2') {
+        await client.sendText(message.from, "Enter your query")
+        client.onMessage(async message => {
+          const query = message.body;
+        });
       }
     });
   });
